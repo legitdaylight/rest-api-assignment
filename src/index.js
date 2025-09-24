5e26db7394a0011e1dd3c6e56cb59f981239596e
Jeremy De La Rosa <jeremyd26937@gmail.com>
2025-09-23 20:02:52 -0400

    completed excerise 3

--- src/index.js
+++ src/index.js
@@ -13,6 +13,56 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+let users = [];
+
+app.post('/users', (req, res) => {
+    if (!req.body.name || !req.body.email) {
+        return res.status(400).json({ error: 'Name and email are required' });
+    }
+    const user = {
+        id: (users.length + 1).toString(),
+        name: req.body.name,
+        email: req.body.email
+    }
+    users.push(user);
+    res.status(201).json(user);
+});
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === req.params.id);
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+    res.status(200).json(user);
+})
+
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+app.delete('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id === req.params.id);
+    if (userIndex === -1) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+    users.splice(userIndex,1);
+    res.sendStatus(204);
+
+})
+
+app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === req.params.id);
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+    if (!req.body.name || !req.body.email) {
+        return res.status(400).json({ error: 'Name and email are required' });
+    }
+    user.email = req.body.email;
+    user.name = req.body.name;
+    res.status(200).json(user);
+})
+
 // Do not touch the code below this comment
 // **************************************************************
 
